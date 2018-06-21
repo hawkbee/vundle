3584150f699737a4ba7c1c2fb27e98cf4fe3410b
Qun He <hawkbee@gmail.com>
2018-06-21 17:32:26 +0800

    Add doom emacs.
    Signed-off-by: Qun He <hawkbee@gmail.com>

---
+++ doom/modules/lang/po/config.el
@@ -0,0 +1,9 @@
+;; (def-package! vue-mode
+;;   :mode "\\.vue$"
+;;   :mode "\\.wpy$"
+;;   :config
+;;   ;; 0, 1, 2 representing (respectively) none, low, and high coloring
+;;   (set! mmm-submode-decoration-level 0))
+
+(def-package! po-mode
+  :mode "\\.po$")
